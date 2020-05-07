4631b2b658e62bebd8503a44c00ad64046508640
root <root@DESKTOP-L2I06VM.localdomain>
2020-05-07 17:07:42 +0300

    added todo list part

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end    
+  end  
+  
   get 'welcome/index'
 
   root "welcome#index"
+  # root "todo_lista#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

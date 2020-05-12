38f5be27ad2d0e213121462984945151d75de7d4
Maram Alfawaz <maram.a.edu@gmail.com>
2020-05-12 08:57:31 +0300

    controllers fixes + css

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def space
-    @favorites = Favorite.all.order('created_at DESC').limit(2)
+    @favorites = Favorite.all.order('created_at DESC').limit(3)
     @notes = Note.all.order('created_at DESC').limit(2)
     @todo_lists = TodoList.all.order('created_at DESC').limit(2)
 

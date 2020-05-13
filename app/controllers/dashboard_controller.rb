e77fb136a62f191bb9be07fdbfda74fbf85ab241
Maram Alfawaz <maram.a.edu@gmail.com>
2020-05-14 01:43:45 +0300

    fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   def space
     @favorites = Favorite.all.order('created_at DESC').limit(3)
-    @notes = Note.all.order('created_at DESC').limit(1)
+    @notes = Note.all.order('created_at DESC').limit(2)
     @todo_lists = TodoList.all.order('created_at DESC').limit(2)
     
   end

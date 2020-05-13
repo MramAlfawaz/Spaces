22a229bcf5601d8d95eed6dc5dc78ce5ed08b3cf
Maram Alfawaz <maram.a.edu@gmail.com>
2020-05-13 05:27:48 +0300

    95% final

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
   def space
     @favorites = Favorite.all.order('created_at DESC').limit(3)
     @notes = Note.all.order('created_at DESC').limit(1)
     @todo_lists = TodoList.all.order('created_at DESC').limit(2)
-
+    
   end
 end

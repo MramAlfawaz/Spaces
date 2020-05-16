790858dfc25f4f8ef3cff267063287e3bc53fe3b
root <root@DESKTOP-L2I06VM.localdomain>
2020-05-16 04:11:02 +0300

    fix TodoList model

---
+++ app/javascript/packs/sweetAlertConfirmDefaults.js
@@ -0,0 +1,8 @@
+var sweetAlertConfirmDefaults = {
+  title: 'Are you sure?',
+  type: 'warning',
+  showCancelButton: true,
+  confirmButtonText: "I'm sure",
+  confirmButtonClass: 'btn btn-danger',
+  cancelButtonClass: 'btn btn-default',
+};
